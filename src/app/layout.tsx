6ed973c552c2f832dd2ebe7d0a886776c3aec54f
Dennis Acosta <email@dennisacosta.me>
2025-09-04 19:34:18 -0400

    Home hero mobile, wip

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,9 @@ export const metadata: Metadata = {
   description: "Dennis Acosta Portfolio",
 };
 
-export default function RootLayout({ children, }: Readonly<{ children: React.ReactNode; }>) {
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
       <body>
