3b854c8798f86b1981e036d6f6fe14bdc72d3631
Dennis Acosta <email@dennisacosta.me>
2025-09-07 18:59:39 -0400

    Update structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
-import type { Metadata } from "next";
 import { GoogleAnalytics } from "@next/third-parties/google";
+
+import type { Metadata } from "next";
 
 import Header from "@components/Header";
 import Footer from "@components/Footer";
